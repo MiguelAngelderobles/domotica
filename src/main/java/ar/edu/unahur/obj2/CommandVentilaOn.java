1fd9d0869eb7be85e844bf310a1184a9b2bac06b
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2019-06-24 19:15:12 -0300

    robe una idea de juan

--- src/main/java/ar/edu/unahur/obj2/CommandVentilaOn.java
+++ src/main/java/ar/edu/unahur/obj2/CommandVentilaOn.java
@@ -2,12 +2,18 @@ package ar.edu.unahur.obj2;
 
 public class CommandVentilaOn implements Command {
     private VentiladorDeTecho ventiladorDeTecho;
-
+    private int cont=0;
     public void executed() {
-        ventiladorDeTecho.baja();
-        ventiladorDeTecho.media();
-        ventiladorDeTecho.fuerte();
-    }
+        cont++;
+        if(cont==1){
+            ventiladorDeTecho.baja();
+        }if(cont==2){
+            ventiladorDeTecho.media();
+        }if (cont==3){
+            ventiladorDeTecho.fuerte();
+            cont=0;
+        }
+       }
 
     public CommandVentilaOn(VentiladorDeTecho ventiladorDeTecho) {
         this.ventiladorDeTecho = ventiladorDeTecho;
